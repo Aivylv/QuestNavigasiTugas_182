0137f8bea88426975f7bb6c3336b340192f8e84d
aivylv <lyvialarasati@gmail.com>
2025-11-09 00:48:35 +0700

    mengaktifkan penggunaan fitur Material3 eksperimental dan menandai fungsi tersebut sebagai komponen UI Jetpack Compose

--- app/src/main/java/com/example/questnavigasitugas_182/view/HalamanHome.kt
+++ app/src/main/java/com/example/questnavigasitugas_182/view/HalamanHome.kt
@@ -21,3 +21,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.questnavigasitugas_182.R
+
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
